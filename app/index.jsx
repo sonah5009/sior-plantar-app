4255411c3327030674172687829443fe5c2dd956
sonah5009 <sonah5009@gmail.com>
2024-11-05 17:28:09 +0900

    button navigator

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,22 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>발 모양과 압력 분석으로 신발 추천받기</Text>
-      <StatusBar />
-      <Link href="/measurePressure" style={styles.link}>
-        발의 압력을 재고 사진을 찍으면 AI가 신발을 추천해드려요.
+      <Text>발의 압력을 재고 사진을 찍으면 AI가 신발을 추천해드려요.</Text>
+      {/* <StatusBar /> */}
+      <Link
+        href="/measurePressure"
+        style={{ marginHorizontal: "auto" }}
+        asChild
+      >
+        <Pressable style={styles.button}>
+          <Text style={styles.buttontext}>measurePressure</Text>
+        </Pressable>
       </Link>
     </View>
   );
@@ -36,6 +43,19 @@ const styles = StyleSheet.create({
     textAlign: "center",
     textDecorationLine: "underline",
     backgroundColor: "rgba(0,0,0,0.5)",
+    padding: 4,
+  },
+  button: {
+    height: 60,
+    borderRadius: 20,
+    backgroundColor: "rgba(0,0,0,0.75)",
+    padding: 6,
+  },
+  buttontext: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
     padding: 4,
   },
 });
