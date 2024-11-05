a0076b0a68d6a27a3e84705c3834f48f584323ca
sonah5009 <sonah500@gmail.com>
2024-11-05 20:09:50 +0900

    add: captureFootSize.jsx

---
+++ app/captureFootSize.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const captureFootSize = () => {
+  return (
+    <View>
+      <Text>captureFootSize</Text>
+    </View>
+  );
+};
+
+export default captureFootSize;
