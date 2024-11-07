d69df3d1b4ee49afd2ec30798394b8c49a8f17e7
sonah5009 <sonah5009@gmail.com>
2024-11-07 22:53:57 +0900

    폰트 추가: BlackHanSans-Regular

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,24 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "BlackHanSans-Regular": require("../assets/fonts/BlackHanSans-Regular.ttf"),
+  });
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <SafeAreaProvider>
       <Stack>
